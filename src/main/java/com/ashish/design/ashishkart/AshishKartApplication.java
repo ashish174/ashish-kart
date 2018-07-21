2de5438f63a7f77a1c07b1347362ad17b12166db
askumar <askumar@akamai.com>
2018-07-21 09:35:11 +0530

    Project setup

---
+++ src/main/java/com/ashish/design/ashishkart/AshishKartApplication.java
@@ -0,0 +1,12 @@
+package com.ashish.design.ashishkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshishKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshishKartApplication.class, args);
+	}
+}
